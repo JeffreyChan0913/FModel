d4d5be84f9ed84be95cf37f6d7048c4a0de20885
Jeffrey Chan <53013457+JeffreyChan0913@users.noreply.github.com>
2024-07-08 22:57:15 -0700

    Add files via upload

--- project.R
+++ project.R
@@ -1,86 +1,182 @@
 setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 required.pkgs <- c("quantmod", "reticulate", "dplyr")
-pacman::p_load(required.pkgs, character.only=T)
 
-download.data <- function(vector.of.tickers){
-  folder <- file.path(Sys.getenv("STATS417"), "project")
+pacman::t_load(required.pkgs, character.only=T)
+
+download.data <- function( vector.of.tickers ){
+  
   use_virtualenv("~/ml", required = T)
-  crawl.tickers <-paste0(paste(vector.of.tickers, collapse=" "))
-  command <- sprintf("~/ml/bin/python3 %s/crawler.py %s", folder, crawl.tickers)
+  
+  prefix        <- Sys.getenv("STATS417")
+  
+  folder        <- file.path( prefix, "project" )
+  
+  crawl.tickers <- paste0( paste( vector.of.tickers, collapse=" " ) )
+  
+  command       <- sprintf("~/ml/bin/python3 %s/crawler.py %s", folder, crawl.tickers)
+  
   system(command) 
+  
 }
 
-# output weight, return, variance, sharpe ratio
 monte.carlo.portfolio <- function(
-    mu.ret, # n by 1
-    portfolio.cov, # n by n 
+    
+    mu.ret,
+    
+    portfolio.cov,
+    
     n,
+    
     iteration = 1000000
+    
 ) {
+  
   risk.free.rate  <- 0.05375
-  result          <- matrix(NA, nrow=iteration, ncol=n+3)
+  
+  result          <- matrix( NA, nrow=iteration, ncol=n+3 )
+  
   for(i in 1:iteration){
-    w           <- runif(n,0,1000)
-    w           <- w / sum(w)
+    
+    w           <- runif( n,0,1000 )
+    
+    w           <- w / sum( w )
+    
     ret         <- w %*% mu.ret
-    vol         <- t(w) %*% portfolio.cov %*% w
-    result[i, ] <- c(w, ret, sqrt(vol), (ret - risk.free.rate) / vol)
+    
+    vol         <- t( w ) %*% portfolio.cov %*% w
+    
+    result[i, ] <- c( w, ret, sqrt( vol ), ( ret - risk.free.rate ) / vol )
+    
   }
-  colnames(result) <- c(paste0("w",1:n), "ret", "sigma", "sharperatio")
-  return (result)
+  
+  colnames( result ) <- c( paste0( "w",1:n ), "ret", "sigma", "sharperatio" )
+  
+  return ( result )
 }
 
 stock.data <- function(
+    
     tickers,
+    
     update=F,
+    
     folder = "data/"
+    
 ) {
-  if(!dir.exists(folder) || update){
-    dir.create(folder)
-    download.data(tickers)
-    return("Done")
+  
+  if( !dir.exists( folder ) || update ){
+    
+    dir.create( folder )
+    
+    download.data( tickers )
+    
+    return( "Done" )
+    
   }
+  
   get.tickers <- c()
-  for(ticker in tickers){
-    file <- paste(ticker,"csv", sep='.')
-    if(!file.exists(file.path(folder, file))){
-      get.tickers <- c(get.tickers, ticker)
+  
+  for( ticker in tickers {
+    
+    file      <- paste( ticker,"csv", sep='.' )
+    
+    ticker.file.path <- file.path( folder, file )
+    
+    if(!file.exists( ticker.file.path ) ) {
+      
+      get.tickers <- c( get.tickers, ticker )
+      
     }
+    
   }
-  if(length(get.tickers) > 0){
-    download.data(get.tickers)
+  
+  if( length(get.tickers) > 0 ){
+    
+    download.data( get.tickers )
+    
   }
+  
 }
 
 process.data <- function(
+    
     tickers,
-    folder = "data"
+    
+    folder    = "data",
+    
+    startDate = as.Date("2015-01-01"),
+    
+    endDate   = as.Date("2024-01-01")
+    
 ) {
-  n         <- length(tickers)
+  
+  n         <- length( tickers )
+  
   mu        <- c()
+  
   var       <- c()
+  
   cov.data  <- c()
-  for(ticker in tickers){
-    ticker.data       <- read.csv(file.path(folder, paste(ticker, "csv", sep=".")))
-    ticker.data$Date  <- as.Date(ticker.data$Date)
-    ticker.data       <- subset(ticker.data, Date >= as.Date("2015-01-01"), Date < as.Date("2024-01-01"))
-    cov.data          <- cbind(cov.data, ticker.data$ret)
-    mu                <- c(mu, mean(ticker.data$ret, na.rm=T))
-    var               <- c(var, var(ticker.data$ret, na.rm=T))
+  
+  for( ticker in tickers ){
+    
+    ticker.file.path  <- file.path( folder, paste( ticker, "csv", sep="." ) )
+    
+    ticker.data       <- read.csv( ticker.file.path )
+    
+    ticker.data$Date  <- as.Date( ticker.data$Date)
+    
+    ticker.data       <- subset( ticker.data,
+                                 Date >= startDate,
+                                 Date < endDate )
+    
+    cov.data          <- cbind( cov.data, ticker.data$ret )
+    
+    ticker.mu         <- mean( ticker.data$ret, na.rm=T )
+    
+    ticker.var        <- var( ticker.data$ret, na.rm=T )
+    
+    mu                <- c( mu, ticker.mu )
+    
+    var               <- c( var, ticker.var )
+    
   }
-  return.package <- list(data.frame("mu"=mu,"var"=var, row.names=tickers), as.matrix(cov.data))
-  return(return.package)
+  
+  df              <- data.frame( "mu"=mu,"var"=var, row.names=tickers )
+  
+  cov.data        <- as.matrix( cov.data )
+  
+  return.package  <- list( df, cov.data )
+  
+  return( return.package )
+  
 }
 
 # tickers <- c("NVDA", "AMD", "TSM", "ASML", "EIX","DUK","AAPL", "TSLA", "CVS","SCI", "UNH","CSV")
-tickers <- c("NVDA", "AMD", "TSM", "ASML", "EIX","DUK","AAPL", "TSLA", "CVS","SCI")
-stock.data(tickers, update=T)
-data.package            <- process.data(tickers)
-stock.stats             <- data.package[[1]]
-stock.cov               <- cov(data.package[[2]])
-row.names(stock.stats)  <- tickers
-row.names(stock.cov)    <- tickers
-colnames(stock.cov)     <- tickers
-result.mc               <- monte.carlo.portfolio(stock.stats$mu, stock.cov, length(tickers))
-plot(result.mc[,"sigma"], result.mc[, "ret"])
+
+tickers <- c(
+              "NVDA", "AMD", "TSM", "ASML", "EIX",
+              "DUK","AAPL", "TSLA", "CVS","SCI"
+            )
+
+stock.data( tickers, update=T )
+
+data.package              <- process.data(tickers)
+
+stock.stats               <- data.package[[1]]
+
+stock.cov                 <- cov(data.package[[2]])
+
+row.names( stock.stats )  <- tickers
+
+row.names( stock.cov )    <- tickers
+
+colnames( stock.cov )     <- tickers
+
+result.mc                 <- monte.carlo.portfolio( stock.stats$mu, 
+                                                    stock.cov, 
+                                                    length( tickers )
+                                                   )
+
+plot( result.mc[,"sigma"], result.mc[, "ret"] )
